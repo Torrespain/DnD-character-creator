85f481bfc48b7cc7efd4e7f10758990236f4aaf8
torrespain <javirorre86@gmail.com>
2018-03-27 12:59:48 -0700

    added home component

--- charcter/src/App.js
+++ charcter/src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import HomePage from './components/character/Character';
+import CharPage from './components/character/Character';
+import Home from './components/home/Home';
 import "./App.css";
 // import AboutPage from './pages/about';
 
+
 class App extends Component {
+	// constructor() {
+	//     super();
+	//     this.state = {
+	//       charView: false
+	//     }
+	//     this.createText = this.createText.bind(this);
+	// }
+
+	// createText() {
+ //    	this.setState({ charView: true }) 
+ //  	}
+
+
+// <button onClick={this.createText}>Create a character</button>
+// 	        {this.state.charView && <p>works!!!</p>}
   render() {
     return (
       <Router>
-        <div>
-          <Route path="/character" component={HomePage} />
+        <div className="{window.location.pathname === '/character' ? characterPage : homePage}">
+	      	<Route path="/" component={Home}/>	        	               
+          <Route path="/character" component={CharPage}/>
         </div>
       </Router>
     );
   }
 }
+
+
 
 export default App;
---
+++ charcter/src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css'
+
+
+class home extends React.Component {
+
+  render() {
+    return (
+        <div className="charButton">
+	    	 <button>Create a character</button>               
+        </div>
+    );
+  }
+}
+
+export default home
