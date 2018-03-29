2d19c1736175849c39f2c5e9e5bfc74bd3ed9048
Ricky Feng <34487908+Rickyfeng7@users.noreply.github.com>
2018-03-28 18:11:13 -0700

    connected to sever and backend

---
+++ backend/models/classes.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema cionstructor, create a new CharacterSchema object
+// This is similar to a Sequelize model
+var classesSchema = new Schema({
+  // `title` is of type String
+  // races: String,
+  // classes: String
+// id: String,
+index: Number,
+name: String,
+hit_die: Number,
+proficiency_choices: "",
+proficiencies:"",
+saving_throws:"",
+starting_equipment:"",
+class_levels:"",
+subclasses:"",
+spellcasting:""
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var dnd = mongoose.model("dnd", classesSchema);
+
+// Export the Character model
+module.exports = dnd;
--- server.js
+++ server.js
@@ -6,9 +6,10 @@ var axios = require("axios");
 
 
 // Require all models
-var db = require("./models");
+var db = require("./backend/models");
 
 
+// console.log(db)
 var port = process.env.PORT || 3005;
 
 var app = express();
@@ -16,17 +17,46 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Import routes and give the server access to them.
-// var routes = require("/");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/dnd", {
+//   useMongoClient: true
+});
 
 // app.use("/",routes);
 
 app.get("/server", function(req, res){
-	console.log("we hit the home route")
-	res.send("working")
+	// console.log("we hit the home route")
+	// console.log('db', db);
+  // console.log('dnd', db.dnd)
+	db.dnd.find({})
+	// res.send("working")
+	.then(function(dnd) {
+		// If any Books are found, send them to the client
+		console.log(dnd)
+		res.json(dnd);
+	  })
+	  .catch(function(err) {
+		// If an error occurs, send it back to the client
+		console.log("err", err);
+		res.json(err);
+	});
 })
+
+app.post("/create", function(req, res){
+	console.log(req.body);
+	db.dnd.create(req.body).then(function(response){
+		console.log("created", response);
+	}).catch(function(err){
+		console.log("err", err);
+	})
+});
 
 app.listen(port, function() {
   console.log("Listening on port:%s", port);
